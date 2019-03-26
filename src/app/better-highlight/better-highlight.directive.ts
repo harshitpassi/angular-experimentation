718820a42bb558b99a64a8146cdf099c97d5a7a5
Harshit Passi <hrshtpassi@gmail.com>
2019-03-26 18:27:11 +0530

    [4] Implemented usage of HostBinding and HostListener

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,30 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input('appBetterHighlight') highlightColor = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor;
 
   constructor(private renderer: Renderer2, private element: ElementRef) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+    this.renderer.setStyle(this.element.nativeElement, 'color', 'black');
   }
 
 }
