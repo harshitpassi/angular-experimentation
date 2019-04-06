6872c750eac1d13dee6e8eafa110398a26784848
Harshit Passi <hrshtpassi@gmail.com>
2019-04-07 01:30:41 +0530

    [Chapter 7] Basic intro to Observables and Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  user1Activated = false;
+  user2Activated = false;
+  userActivationSubscription: Subscription;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.userActivationSubscription = this.usersService.userActivated.subscribe( (id: number) => {
+      switch (id) {
+        case 1 : this.user1Activated = true;
+        break;
+        case 2 : this.user2Activated = true;
+        break;
+      }
+    } );
+  }
+
+  ngOnDestroy() {
+    this.userActivationSubscription.unsubscribe();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer, Subscription, interval } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  mySubscription: Subscription;
+  numberSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = interval(1000).pipe(map( (data: number) => data * 2 )).pipe(filter( (data: number) => data % 10 !== 0 ));
+    this.numberSubscription = myNumbers.subscribe((num: number) => console.log(num));
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout( () => observer.next('first data packet'), 2000);
+      setTimeout( () => observer.next('second data packet'), 4000);
+      setTimeout( () => observer.error('Failed and thrown error'), 5000);
+      // setTimeout( () => observer.complete(), 5000);
+      setTimeout( () => observer.next('third data packet'), 6000); // Never emitted as the Observable is completed before it.
+    });
+
+    this.mySubscription = myObservable.subscribe( (str: string) => console.log(str), // normal data
+      (e: string) => console.log(e), // error handling
+      () => console.log('Completed') ); // completion handling
+  }
+
+  ngOnDestroy() {
+    this.mySubscription.unsubscribe();
+    this.numberSubscription.unsubscribe();
   }
 
 }
